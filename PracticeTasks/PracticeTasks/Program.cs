f7bec5182a7fc03a960f54ff8d8b1c269b90b3cd
[Asgar Babayev] <[tu7609td3@code.edu.az]>
2022-03-04 23:52:04 +0400

    added tasks

---
+++ PracticeTasks/PracticeTasks/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PracticeTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //Console.Write("Array-a ölçü verin: ");
+            //int arrSize = Convert.ToInt32(Console.ReadLine());
+            //if (arrSize > 0)
+            //{
+            //    int[] arr = new int[arrSize];
+            //    for (int i = 0; i < arrSize; i++)
+            //    {
+            //        Console.Write(i + ". " + "elementi daxil edin: ");
+            //        arr[i] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //    int minEl = arr[0];
+            //    for (int i = 0; i < arr.Length; i++)
+            //        if (arr[i] < minEl) minEl = arr[i];
+
+            //    Console.WriteLine("Arrayın ən kiçik elementi {0}", minEl);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Arrayın ölçüsü 0 ya da mənfi ola bilməz");
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region Task 2
+            //string input = Console.ReadLine();
+            //bool isHaveSymbol = false;
+            ////int count = 0;                         <--Əgər a simvolunun sayını tapmaq istəsək count-dan istifadə edə bilərik.-->
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    if (input[i] == 'a')
+            //    {
+            //        //count++;
+            //        isHaveSymbol = true;
+            //        break;  
+            //    }
+            //}
+            //Console.WriteLine($"a simvolu varsa true, yoxdursa false qaytarır: {isHaveSymbol}");
+            ////Console.WriteLine($"Mətndəki a simvolunun sayı = {count}");
+            //Console.ReadKey();
+            #endregion
+        }
+    }
+}
